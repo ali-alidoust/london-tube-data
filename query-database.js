884fd2bbc8118684b3061c52197c8569a2b84158
Ali Alidoust <ali.alidoust@gmail.com>
2022-11-11 20:32:00 +0200

    Implemented getLines method

--- query-database.js
+++ query-database.js
@@ -1,3 +1,45 @@
+const mysql = require("mysql");
+const util = require("util");
+const { exit } = require("process");
+
+async function getConnection() {
+  return mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "pass12345", //TODO: Use .env file
+    database: "london",
+  });
+}
+
+async function getStations(lineId) {
+  const con = await getConnection();
+  const query = util.promisify(con.query).bind(con);
+
+  con.end();
+}
+
+async function getLines(stationId) {
+  let con = null;
+  try {
+    con = await getConnection();
+    const query = util.promisify(con.query).bind(con);
+
+    result = await query(`
+        SELECT station_lines.Name FROM station_to_line
+        INNER JOIN station_lines ON station_lines.ID = station_to_line.LineId
+        WHERE station_to_line.StationId = "${stationId}"
+    `);
+
+    return result.map(item => item.Name);
+  } catch (err) {
+    throw err;
+  } finally {
+    if (con) {
+      con.end();
+    }
+  }
+}
+
 async function main() {
   const inputCommand = process.argv[2];
   const inputId = process.argv[3];
@@ -12,16 +54,12 @@ async function main() {
     exit();
   }
 
-  const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "pass12345", //TODO: Use .env file
-    database: "london",
-  });
-
-  const query = util.promisify(con.query).bind(con);
-
-  con.end();
+  if (inputCommand == "getLines") {
+    const result = await getLines(inputId);
+    for (item of result) {
+        console.log(item)
+    }
+  }
 }
 
 main();
