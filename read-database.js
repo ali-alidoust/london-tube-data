3ac6f32b58de5a13429f007ae4c4527fd1805d35
Ali Alidoust <ali.alidoust@gmail.com>
2022-11-11 19:47:59 +0200

    Implemented data insertion

--- read-database.js
+++ read-database.js
@@ -29,19 +29,19 @@ async function main() {
 
   const data = loadDataFromFile(inputFileName);
 
+  await query(`DROP TABLE IF EXISTS station_to_line;`);
   await query(`DROP TABLE IF EXISTS stations;`);
   await query(`DROP TABLE IF EXISTS station_lines;`);
-  await query(`DROP TABLE IF EXISTS station_to_line;`);
 
   await query(
     `
       CREATE TABLE stations (
-        ID CHAR(11) NOT NULL UNIQUE,
+        ID CHAR(12) NOT NULL UNIQUE,
         Name VARCHAR(255) NOT NULL
       );
     `
   );
-  
+
   await query(
     `
       CREATE TABLE station_lines (
@@ -54,13 +54,34 @@ async function main() {
   await query(
     `
       CREATE TABLE station_to_line (
-        StationId CHAR(11) NOT NULL,
+        StationId CHAR(12) NOT NULL,
         LineId int NOT NULL,
         FOREIGN KEY (StationId) REFERENCES stations(ID),
         FOREIGN KEY (LineId) REFERENCES station_lines(ID)
       );
     `
   );
+
+  for (const station of data.stations) {
+    await query(
+      `INSERT INTO stations (ID, Name) VALUES ("${station.id}","${station.name}");`
+    );
+  }
+
+  let lineId = 0;
+  for (const line of data.lines) {
+    await query(
+      `INSERT INTO station_lines (ID, Name) VALUES (${lineId}, "${line.name}");`
+    );
+
+    for (const stationId of line.stations) {
+      await query(
+        `INSERT INTO station_to_line (StationId, LineId) VALUES ("${stationId}", ${lineId});`
+      );
+    }
+
+    lineId++;
+  }
 
   con.end();
 }
